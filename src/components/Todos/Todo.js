ff5b3d14e4db3b63bac07dc2062f1ee4fe916e9e
Alexandr <golubovskiy.al@mail.ru>
2022-11-20 15:40:07 +0300

    feat: add change and delete buttons to todo

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,4 +1,5 @@
-import { RiTodoFill } from 'react-icons/ri';
+import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 function Todo(props) {
@@ -7,6 +8,8 @@ function Todo(props) {
     <div onDoubleClick={() => deleteTodo(todo.id)} className={styles.todo}>
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line className={styles.checkIcon} />
+      <FaCheck className={styles.deleteIcon} />
     </div>
   );
 }
