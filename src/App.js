d57aabdf23220bacf104f304a86c22b61261529c
Alexandr <golubovskiy.al@mail.ru>
2022-11-20 16:51:40 +0300

    feat: add active status for delete button

--- src/App.js
+++ src/App.js
@@ -39,19 +39,29 @@ function App() {
     setTodos(todos.filter((todo) => !todo.isCompleted));
   };
 
+  const completedTodosCount = todos.filter((todo) => todo.isCompleted).length;
+
   return (
     <div className="App">
       <h1>Todo App</h1>
       <TodoForm addTodo={addTodoHandler} />
-      <TodosActions
-        resetTodos={resetTodosHandler}
-        deleteCompletedTodos={deleteCompletedTodosHandler}
-      />
+      {todos.length > 0 && (
+        <TodosActions
+          completedTodosExist={!!completedTodosCount}
+          resetTodos={resetTodosHandler}
+          deleteCompletedTodos={deleteCompletedTodosHandler}
+        />
+      )}
       <TodoList
         todos={todos}
         deleteTodo={deleteTodoHandler}
         toggleTodo={toggleTodoHandler}
       />
+      {completedTodosCount > 0 && (
+        <h2>{`You have completed ${completedTodosCount} ${
+          completedTodosCount > 1 ? 'todos' : 'todo'
+        }`}</h2>
+      )}
     </div>
   );
 }
--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -1,19 +1,25 @@
 import { RiDeleteBin2Line, RiRefreshLine } from 'react-icons/ri';
 import Button from '../UI/Button';
+import styles from './TodosActions.module.css';
 
-function TodosActions({ resetTodos, deleteCompletedTodos }) {
+function TodosActions({
+  resetTodos,
+  deleteCompletedTodos,
+  completedTodosExist,
+}) {
   return (
-    <>
+    <div className={styles.todosActionsContainer}>
       <Button title="Reset Todos" onClick={() => resetTodos()}>
         <RiRefreshLine />
       </Button>
       <Button
         title="Clear completed todos"
         onClick={() => deleteCompletedTodos()}
+        disabled={!completedTodosExist}
       >
         <RiDeleteBin2Line />
       </Button>
-    </>
+    </div>
   );
 }
 
